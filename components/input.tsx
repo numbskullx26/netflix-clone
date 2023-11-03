9843cb3846ba247d2ca473c65c72eab10cd545d3
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2023-11-04 00:19:36 +0530

    add : project folder structure

---
+++ components/input.tsx
@@ -0,0 +1,18 @@
+const Input = () => {
+  return (
+    <div className="relative">
+        <input id="email" className="block rounded-md bg-neutral-700 text-white px-6 pt-6 pb-1 w-full text-md appearance-none focus:outline-none focus:ring-0
+    peer" placeholder="" />
+
+    <label htmlFor="email" className=""></label>
+    </div>
+  );
+};
+
+export default Input;
+
+{/* <input
+  className="block rounded-md bg-neutral-700 text-white px-6 pt-6 pb-1 w-full text-md appearance-none focus:outline-none focus:ring-0
+peer"
+  placeholder=""
+/> */}
